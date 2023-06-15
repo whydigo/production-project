6553060483945046258297d0e5ddffb4509d13b7
whydigo <ukunsss7@gmail.com>
2023-06-15 16:07:50 +0300

    Navbar, создание отдельных тем для ссылок, создание шаблона, первый ui kit компонент

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import './styles/index.scss'
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 
 const App = () => {
@@ -10,10 +11,9 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     );
 };
