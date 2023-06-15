d4d179c41675f17d2e69539edf03bb45c2b14459
whydigo <ukunsss7@gmail.com>
2023-06-15 13:47:45 +0300

    добавление конфига для роутера

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import { Suspense } from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './styles/index.scss'
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import { AppRouter } from 'app/providers/router';
+
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
@@ -14,12 +13,7 @@ const App = () => {
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />}/>
-                    <Route path={'/'} element={<MainPage />}/>
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
